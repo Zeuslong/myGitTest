4efe8fa3b8df1406aa6e6e909d638c508953e455
linglong <895094278@qq.com>
2019-08-11 16:36:50 +0800

    测试用列

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author linglong
+ * @date 2019/8/11
+ * @Description
+ **/
+public class Test {
+    public static void main(String[] args) {
+        int a =20;
+        System.out.println("zh");
+    }
+}
