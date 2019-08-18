57d28c360f5c4b7c25f710ed16f49ef3cf5bda5d
linglong <895094278@qq.com>
2019-08-18 15:48:43 +0800

    myGitTest  to origin  test merge

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,9 +8,16 @@ public class Test {
         int a =20;
         int c=30;
 
+        int d=40;
+        System.out.println("this is myGitTest");
+
         int b=50;
         System.out.println("test git reset --hard 4efe8fa");
 
         System.out.println("zh");
+    }
+
+    public void domyOwn(Integer integer){
+        System.out.println("this is a test of myGitTest");
     }
 }
