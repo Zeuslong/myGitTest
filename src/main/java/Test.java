8848c7c9d92fdfa2d53cb0c97e307d7dcd8ef0a8
linglong <895094278@qq.com>
2019-08-18 15:44:47 +0800

    myGitTest_develop  Test  merge

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,10 +7,15 @@ public class Test {
     public static void main(String[] args) {
         int a =20;
         int c=30;
+        int e=40;
 
         int b=50;
         System.out.println("test git reset --hard 4efe8fa");
 
         System.out.println("zh");
+    }
+
+    public void test(){
+        System.out.println("tihs  is a merge test");
     }
 }
